c4f2b87217522cff82ed1dd105b633ef54511266
Mishrashubham10 <shubhammishra10101998@gmail.com>
2023-03-30 06:56:46 +0530

    pushing chages

--- js/app.js
+++ js/app.js
@@ -26,4 +26,15 @@ kits.forEach((kit) => {
     btn.addEventListener("click", () => {
         audio.play();
     });
+
+    window.addEventListener("keydown", (e) => {
+        if (e.key === kit.slice(0,1)) {
+            audio.play();
+            btn.style.transform = "scale(.9)";
+            
+            setTimeout(() => {
+                btn.style.transform = "scale(1)";
+            }, 2000);
+        };
+    });
 });
